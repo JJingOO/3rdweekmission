a8d7ac1a389c456115666b3284d9dcc9704d7079
sam <wlsrnsk1243@naver.com>
2023-03-29 16:53:48 +0900

    3주차 미션 마무리

--- app/src/main/java/com/cookandroid/a3rdweekmission/MainActivity.java
+++ app/src/main/java/com/cookandroid/a3rdweekmission/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         btnUD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "지난 겨울에 친구들과 함께 놀러가서 찍은 사진입니다 ^^ 제출이 늦어서 죄송합니다. 최선을 다하겠습니다!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "지난 겨울에 친구들과 함께 놀러가서 찍은 사진입니다 ^^ 제출이 늦어서 죄송합니다. 최선을 다하겠습니다! - 2021041086 김진구",Toast.LENGTH_SHORT).show();
                 i++;
                 if (i % 2 == 1) {
                     photo2.setImageResource(R.drawable.jingu);
